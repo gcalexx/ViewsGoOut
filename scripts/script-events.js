722af61ba043fb4b7de1e06a8bc6d64f2eaeffe6
gcalexx <paco0cr7@gmail.com>
2018-11-06 07:03:32 -0600

    usuario

--- scripts/script-events.js
+++ scripts/script-events.js
@@ -72,6 +72,74 @@ $(document).ready(function(){
     $('#popup').fadeOut(500);
   }); 
   
+  $("#profile-picture").mouseenter(function(){
+      $("#picture, #label-picture").fadeIn();
+  });
+
+  $("#picture, #label-picture").mouseleave(function(){
+    $("#picture, #label-picture").fadeOut();
+  });
+
+  $("#eventos, #eventos-xs").click(function(){
+    console.log("holaaaa");
+    $("#event-request, #my-friends, #friend-request").css("animation-name", "fadeOut");
+    $("#event-request, #my-friends, #friend-request").css("animation-duration", ".5s");
+    setTimeout(function(){
+      $("#event-request, #my-friends, #friend-request").hide();
+      $("#event-request, #my-friends, #friend-request").css("animation-name", "");
+      $("#event-request, #my-friends, #friend-request").css("animation-duration", "");
+    }, 430);
+    setTimeout(function(){
+      $("#my-events").css("animation-name", "fadeIn");
+      $("#my-events").css("animation-duration", "1s");
+      $("#my-events").show();
+    }, 600);
+  });
+
+  $("#invitaciones, #invitaciones-xs").click(function(){
+    $("#my-events, #my-friends, #friend-request").css("animation-name", "fadeOut");
+    $("#my-events, #my-friends, #friend-request").css("animation-duration", ".5s");
+    setTimeout(function(){
+      $("#my-events, #my-friends, #friend-request").hide();
+      $("#my-events, #my-friends, #friend-request").css("animation-name", "");
+      $("#my-events, #my-friends, #friend-request").css("animation-duration", "");
+    }, 430);
+    setTimeout(function(){
+      $("#event-request").css("animation-name", "fadeIn");
+      $("#event-request").css("animation-duration", "1s");
+      $("#event-request").show();
+    }, 600);
+  });
+
+  $("#amigos, #amigos-xs").click(function(){
+    $("#my-events, #event-request, #friend-request").css("animation-name", "fadeOut");
+    $("#my-events, #event-request, #friend-request").css("animation-duration", ".5s");
+    setTimeout(function(){
+      $("#my-events, #event-request, #friend-request").hide();
+      $("#my-events, #event-request, #friend-request").css("animation-name", "");
+      $("#my-events, #event-request, #friend-request").css("animation-duration", "");
+    }, 430);
+    setTimeout(function(){
+      $("#my-friends").css("animation-name", "fadeIn");
+      $("#my-friends").css("animation-duration", "1s");
+      $("#my-friends").show();
+    }, 600);
+  });
+
+  $("#solicitudes, #solicitudes-xs").click(function(){
+    $("#my-events, #event-request, #my-friends").css("animation-name", "fadeOut");
+    $("#my-events, #event-request, #my-friends").css("animation-duration", ".5s");
+    setTimeout(function(){
+      $("#my-events, #event-request, #my-friends").hide();
+      $("#my-events, #event-request, #my-friends").css("animation-name", "");
+      $("#my-events, #event-request, #my-friends").css("animation-duration", "");
+    }, 430);
+    setTimeout(function(){
+      $("#friend-request").css("animation-name", "fadeIn");
+      $("#friend-request").css("animation-duration", "1s");
+      $("#friend-request").show();
+    }, 600);
+  });
 
 });
 
