9167e331658de61b0fae454546959b40d8366021
gcalexx <paco0cr7@gmail.com>
2018-11-16 03:34:54 -0600

    Quitando down xd

--- scripts/script-events.js
+++ scripts/script-events.js
@@ -147,31 +147,19 @@ $(document).ready(function(){
   });
 
   /*************slider**********/
-  // find all slides
   var slides = $('.main-slide');
-  // starting index
   var i = 0;
-  // click listener
 
   $('#main-slider-next').click(function(){
-    // find next index
-    // i + 1 or 0 if end of slides
     i = ++i % slides.length;
-    // scroll to that index
-    
-       $('.slider-wrapper').animate(
+    $('.slider-wrapper').animate(
       {'left' : -(slides.eq(i).position().left)},
       600
-    );
-    
-    
+    );   
   });
+
   $('#main-slider-prev').click(function(){
-    // find next index
-    // i + 1 or 0 if end of slides
     i = --i % slides.length;
-    
-    // scroll to that index
     $('.slider-wrapper').animate(
       {'left' : -(slides.eq(i).position().left)},
       600
